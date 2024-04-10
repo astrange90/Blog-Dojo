c0809d1e76ed3dc5a98ff1be96ee569da609ed8d
astrange90 <astrange90@hotmail.com>
2024-04-09 21:58:50 -0700

    added npm packages and folder structure for mvc

---
+++ utils/helpers.js
@@ -0,0 +1,14 @@
+module.exports = {
+    format_date: (date) => {
+      // Format date as MM/DD/YYYY
+      return date.toLocaleDateString();
+    },
+  
+    compare: function (v1, comparator, v2) {
+      if (eval(v1 + comparator + v2)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
